1e87c8c4cedc42e64a53c9a2045951b5febe7945
Fonseca <126025204+abigalfonseca@users.noreply.github.com>
2024-04-27 09:06:06 +0100

    Updated

---
+++ reveal.js
@@ -0,0 +1,130 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+//TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo', {
+    duration: 1000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+// TITULO DAS SESSÕES
+
+revelar.reveal('.titulo1', {
+    duration: 1000,
+    distance: '90px'
+})
+
+// ESPECIALIDADES
+
+revelar.reveal('.efeito-especialidade1', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 500
+})
+revelar.reveal('.efeito-especialidade2', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 1000
+})
+revelar.reveal('.efeito-especialidade3', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 1500
+})
+
+
+// SOBRE
+
+revelar.reveal('.efeito-texto-sobre', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'right',
+    delay: 500
+})
+
+revelar.reveal('.efeito-img-sobre', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'left',
+    delay: 1000
+})
+
+// PORTIFÓLIO
+
+revelar.reveal('.efeito-img-port1', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'rigt',
+    delay: 500
+})
+
+revelar.reveal('.efeito-img-port2', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'right',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-img-port3', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'right',
+    delay: 1500
+})
+
+// FORMULÁRIO
+
+
+revelar.reveal('.efeito-btn-form', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 500
+})
+
+
+revelar.reveal('.efeito-form5', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-form4', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-form3', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 2000
+})
+revelar.reveal('.efeito-form2', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 2500
+})
+
+revelar.reveal('.efeito-form1', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 3000
+})
+
+
